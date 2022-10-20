0b73323bad6d780cebd30aaa6982c6ece54664e4
Christian Sanchez <52680827+bigolchris@users.noreply.github.com>
2022-10-20 14:30:21 -0700

    scss styles and marquee added

---
+++ main.js
@@ -0,0 +1,15 @@
+const root = document.documentElement;
+
+const marqueeElementsDisplayed = getComputedStyle(root).getPropertyValue(
+  "--marquee-elements-displayed"
+);
+
+const marqueeContent = document.querySelector("div.data-container");
+
+root.style.setProperty("--marquee-elements", marqueeContent.children.length);
+
+for (let i = 0; i < marqueeElementsDisplayed.length; i++) {
+  marqueeContent.appendChild(marqueeContent.children[i].cloneNode(true));
+}
+
+console.log(root);
