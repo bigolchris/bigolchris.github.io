1222685b48b56c284f36f076043746359bd9e21e
Christian Sanchez <52680827+bigolchris@users.noreply.github.com>
2022-10-20 17:13:32 -0700

    marquee scroll loop working

--- main.js
+++ main.js
@@ -1,15 +1,17 @@
-const root = document.documentElement;
+// const root = document.documentElement;
 
-const marqueeElementsDisplayed = getComputedStyle(root).getPropertyValue(
-  "--marquee-elements-displayed"
-);
+// const marqueeElementsDisplayed = getComputedStyle(root).getPropertyValue(
+//   "--marquee-elements-displayed"
+// );
 
-const marqueeContent = document.querySelector("div.data-container");
+// const marqueeContent = document.querySelector("div.data-container");
 
-root.style.setProperty("--marquee-elements", marqueeContent.children.length);
+// root.style.setProperty("--marquee-elements", marqueeContent.children.length);
 
-for (let i = 0; i < marqueeElementsDisplayed.length; i++) {
-  marqueeContent.appendChild(marqueeContent.children[i].cloneNode(true));
-}
+// for (let i = 0; i < marqueeElementsDisplayed; i++) {
+//   marqueeContent.appendChild(marqueeContent.children[i].cloneNode(true));
+// }
 
-console.log(root);
+// console.log(root);
+
+// console.log(marqueeContent);
