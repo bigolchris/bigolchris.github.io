b1712c8d060d4d4947104941594f61aafed83822
Christian Sanchez <52680827+bigolchris@users.noreply.github.com>
2022-11-01 16:29:41 -0700

    more for smaller displays

--- main.js
+++ main.js
@@ -15,3 +15,30 @@
 // console.log(root);
 
 // console.log(marqueeContent);
+
+// let nav = document.querySelector(".nav-menu");
+
+const navToggle = document.querySelector(".nav-toggle");
+const nav = document.querySelector(".nav-menu");
+
+navToggle.addEventListener("click", () => {
+  document.body.classList.toggle("nav-open");
+});
+
+navToggle.addEventListener("click", () => {
+  if (nav.classList.contains("hide")) {
+    nav.classList.add("show");
+    nav.classList.remove("hide");
+  } else {
+    nav.classList.remove("show");
+    nav.classList.add("hide");
+  }
+});
+// navToggle.addEventListener("click", () => {
+//   if (nav.classList.contains("show")) {
+//     nav.classList.remove("show");
+//     nav.classList.add("nav-open");
+//   } else {
+//     nav.classList.remove("show");
+//   }
+// });
